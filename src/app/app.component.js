482aba77172278e7ab2074cec812b03fcfec2c19
Nuno Jacinto <nuno.jacinto@innowave.tech>
2018-10-01 16:25:58 +0100

    step4 done

---
+++ src/app/app.component.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import env from '@env'
+
+const App = () => 
+    console.log({env}) || 
+    (
+        <div>
+            <h1>Hello dudes, such world!</h1>
+        </div>
+    );
+
+export default App;
